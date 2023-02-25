5136693e082bc2f68c7433b392b70ad980f503d7
santiago9589 <santiagodejesus587@gmail.com>
2023-02-25 18:05:23 -0300

    clean files

--- api/api.ts
+++ api/api.ts
@@ -3,34 +3,20 @@ import { Root } from "types/api";
 
 
 export const api = {
-    list: async (): Promise<Root[] | any> => {
-        try {
+    list: async (): Promise<Root[]> => {
             const response = await axios.get("https://restcountries.com/v2/all")
             return response.data
-        } catch (error: any) {
-            throw new Error(error)
-        }
     },
     country: async (name: string): Promise<Root | any> => {
-        try {
             const response = await axios.get(`https://restcountries.com/v2/name/${name}`)
             return response.data[0]
-        } catch (error: any) {
-            throw new Error(error)
-        }
     },
     routes: async (): Promise<string[] | any> => {
-        try {
             const response = await axios.get("https://restcountries.com/v2/all")
             const routes: string[] = response.data.reduce((prev: string[], current: Root) => {
                 prev.push(current.name)
                 return prev
             }, [])
-
             return routes
-
-        } catch (error: any) {
-            throw new Error(error)
-        }
     }
 }
